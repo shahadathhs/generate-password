beee81cbd4b55b0b4a97e5c8da755c742feeabb2
shahadathhs <shahadathhossensajib732@gmail.com>
2025-01-17 09:51:00 +0600

    feat: jest config added

---
+++ jest.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} **/
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testMatch: ['**/**/*.test.ts'],
+  moduleFileExtensions: ['js', 'ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  verbose: true,
+  forceExit: true,
+  detectOpenHandles: true,
+  clearMocks: true,
+  resetMocks: true,
+  restoreMocks: true,
+  testTimeout: 40000,
+  maxWorkers: 3,
+  silent: false
+}
